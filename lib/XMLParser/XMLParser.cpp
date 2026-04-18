cae53db49ff890178063958e392bb26112ca5716
steve0-t <stefan.simai@mailbox.org>
2026-04-18 22:28:47 +0200

    implement xml parser

--- lib/XMLParser/XMLParser.cpp
+++ lib/XMLParser/XMLParser.cpp
@@ -1,11 +1,84 @@
 #include "XMLParser.h"
 #include "Arduino.h"
 
+XMLParser::XMLParser() : m_cmdType(TCOUNT), m_state(SCOUNT) {}
+
+Retval XMLParser::parse(const rstr& input) {
+    if (!isXML(input))
+        return BAD_FORMAT;
+    char *beg, *cpy, *curr_char, *prev_char;
+    char  buf[256] = {0};
+    beg = cpy = input._str;
+    u8 idx = 0, buf_idx = 0;
+    i8 specification = 0;
+
+    Serial.println(cpy);
+    while ((curr_char = cpy++) != nullptr) {
+        // Serial.print(*curr_char);
+        Serial.println(buf);
+        // Serial.println(specification);
+        if (idx == 0 && *curr_char == '!')
+            return COMMENT;
+
+        if (*curr_char == CLOSING_BRACKET)
+            break;
+
+        if (*curr_char == WHITESPACE) {
+            if (idx == 0) {
+                if (strcmp(buf, "cmd") != 0)
+                    return BAD_FORMAT;
+            }
+            memset((void*)buf, 0, sizeof(buf));
+            buf_idx = 0;
+            idx++;
+            continue;
+        }
+
+        if (*curr_char == OPENING_BRACKET)
+            continue;
+
+        if (*curr_char == '"' && buf[buf_idx - 1] == ASSIGNMENT) {
+            specification = 1;
+            if (strcmp(buf, "type=") == 0 || strcmp(buf, "state=") == 0) {
+                memset((void*)buf, 0, sizeof(buf));
+                buf_idx = 0;
+                continue;
+            } else
+                return BAD_FORMAT;
+        } else if (*curr_char == '"' && specification == 0)
+            return BAD_FORMAT;
+        else if (*curr_char == '"' && specification == 1) {
+            specification = 0;
+            if (idx == 1) {
+                for (i8 i = 0; i < TCOUNT; i++) {
+                    if (strcmp(buf, types[i]) == 0)
+                        this->m_cmdType = (Type)i;
+                }
+                if (this->m_cmdType == TCOUNT)
+                    return UNKNOWN_CMD;
+            }
+
+            if (idx == 2) {
+                for (i8 i = 0; i < SCOUNT; i++) {
+                    if (strcmp(buf, states[i]) == 0)
+                        this->m_state = (State)i;
+                }
+                if (this->m_state == SCOUNT)
+                    return BAD_STATE;
+            }
+            continue;
+        }
+
+        // prev_char      = curr_char;
+        buf[buf_idx++] = *curr_char;
+    }
+    return VALID;
+}
+
 i8 XMLParser::isXML(const rstr& input) {
     if (input._str == nullptr)
         return -1;
     // Serial.println(input._str[0]);
     // Serial.println(input._str[input.len - 1]);
-    return input.front() == OPENING_BRACKET &&
-        input.back() == CLOSING_BRACKET;
+    return input.front() == OPENING_BRACKET && input.back() == CLOSING_BRACKET;
 }
--- lib/XMLParser/XMLParser.h
+++ lib/XMLParser/XMLParser.h
@@ -7,16 +7,50 @@
 #define OPENING_BRACKET '<'
 #define CLOSING_BRACKET '>'
 #define CLOSING_DELIM   '/'
+#define WHITESPACE      ' '
+#define ASSIGNMENT      '='
+
+enum Type {
+    SET = 0,
+    GET,
+    PING,
+    TCOUNT
+};
+
+static const char* types[TCOUNT] = {"set", "get", "ping"};
+
+enum State {
+    STOP = 0,
+    READY,
+    GO,
+    CAUTION,
+    OFF,
+    SCOUNT
+};
+
+static const char* states[SCOUNT] = {"STOP", "READY", "GO", "CAUTION", "OFF"};
+
+enum Retval {
+    BAD_FORMAT  = 1,
+    UNKNOWN_CMD = 2,
+    BAD_STATE   = 3,
+    VALID       = 33,
+    COMMENT
+};
+
+static const char* responses[3] = {"BAD_FORMAT", "UNKNOWN_CMD", "BAD_STATE"};
 
 class XMLParser {
   public:
-    XMLParser() {};
-    ~XMLParser() {};
+    XMLParser();
+    ~XMLParser();
 
-    i8   isXML(const rstr& input);
-    void parse(const rstr* input);
+    i8     isXML(const rstr& input);
+    Retval parse(const rstr& input);
 
   private:
+    Type  m_cmdType;
+    State m_state;
 };
 
 #endif
