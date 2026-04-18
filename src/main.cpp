4a7cfd775787b9d816f82ce93754908b380f1fda
steve0-t <stefan.simai@mailbox.org>
2026-04-19 01:08:50 +0200

    clean up main, implement more sem functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
+#include "Arduino.h"
 #include "HardwareSerial.h"
 #include "pins_arduino.h"
-#include <Arduino.h>
 #include <avr/interrupt.h>
 #include "../lib/Rstr/Rstr.h"
 #include "../lib/XMLParser/XMLParser.h"
+#include "../lib/Pins/Pins.h"
 
-int main() {
+#define RESPONSE_BUFFER_SIZE 128
+
+void sem_off();
+void stop_sem();
+void ready_sem();
+void go_sem();
+i8   set_state(char* response_buffer, const State state, const char* state_str);
+void provide_err_msg(Retval id, char* response_buffer);
+
+int  main() {
     init();
 
 #if defined(USBCON)
@@ -16,44 +26,97 @@ int main() {
 
     XMLParser parser = XMLParser();
     rstr      input  = {._str = (char*)malloc(256 * sizeof(char)), .len = 0};
-
     Retval    res;
+    i8        caution_sem                           = 0;
+    char      response_buffer[RESPONSE_BUFFER_SIZE] = {0};
+
+    // blinking led stuff
+    i8        caution_led_state = 0;
+    u32       previous_milis = 0, current_millis = 0;
+    const u32 blink_interval = 1000;
+
     for (;;) {
+        input.clear_str();
+        parser.reset();
+        if (caution_sem) {
+            current_millis = millis();
+            if (current_millis - previous_milis >= blink_interval) {
+                previous_milis    = current_millis;
+                caution_led_state = (caution_led_state == 0) ? 1 : 0;
+                digitalWrite(YELLOW, caution_led_state);
+            }
+        }
+
         if (Serial.available()) {
             input.len = (u64)Serial.readBytesUntil('\n', input._str, 256);
             if ((res = parser.parse(input)) < VALID) {
-                Serial.println("invalid!");
-                if (res > 0 && res < VALID) {
-                    char response_buffer[128] = {0};
-                    snprintf(response_buffer, 128,
-                             "<rsp status=\"error\" code=\"%s\"/>",
-                             responses[res - 1]);
-                }
+                // Serial.println("invalid!");
+                if (res > 0 && res < VALID)
+                    provide_err_msg(res, response_buffer);
             } else {
                 if (res == COMMENT)
                     Serial.println("Comment");
-                Serial.println("valid!");
+
+                if (parser.getCmpType() == SET) {
+                    caution_sem = set_state(response_buffer, parser.getState(),
+                                            parser.getFields().state);
+                } else if (parser.getCmpType()) {
+                    memset(response_buffer, 0, sizeof(response_buffer));
+                    snprintf(response_buffer, 128,
+                             "<rsp status=\"ok\" state=\"%s\"/>",
+                             parser.getFields().state);
+                    Serial.println(response_buffer);
+                }
             }
-            // if (parser.isXML(input)) {
-            //     Serial.println("is xml");
-            //     digitalWrite(A0, HIGH);
-            //     delay(2000);
-            //     digitalWrite(A0, LOW);
-            // }
-            input.clear_str();
-            // if (input.equals("hello")) {
-            //     Serial.println("secret discovered");
-            //     digitalWrite(A0, HIGH);
-            //     delay(2000);
-            //     digitalWrite(A0, LOW);
-            // } else {
-            //     digitalWrite(A1, HIGH);
-            //     delay(2000);
-            //     digitalWrite(A1, LOW);
-            // }
         }
-        free(input._str);
     }
 
+    free(input._str);
     return 0;
+}
+
+void sem_off() {
+    digitalWrite(RED, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(GREEN, LOW);
+}
+
+void stop_sem() {
+    sem_off();
+    digitalWrite(RED, HIGH);
+}
+
+void ready_sem() {
+    sem_off();
+    digitalWrite(RED, HIGH);
+    digitalWrite(YELLOW, HIGH);
+}
+void go_sem() {
+    sem_off();
+    digitalWrite(GREEN, HIGH);
+}
+
+i8 set_state(char* response_buffer, const State state, const char* state_str) {
+    i8 ret = 0;
+    switch (state) {
+        case STOP: stop_sem(); break;
+        case READY: ready_sem(); break;
+        case GO: go_sem(); break;
+        case OFF: sem_off(); break;
+        case CAUTION: ret = 1; break;
+        default: break;
+    }
+
+    memset(response_buffer, 0, RESPONSE_BUFFER_SIZE);
+    snprintf(response_buffer, RESPONSE_BUFFER_SIZE,
+             "<rsp status=\"ok\" state=\"%s\"/>", state_str);
+    Serial.println(response_buffer);
+    return ret;
+}
+
+void provide_err_msg(Retval id, char* response_buffer) {
+    memset(response_buffer, 0, RESPONSE_BUFFER_SIZE);
+    snprintf(response_buffer, RESPONSE_BUFFER_SIZE,
+             "<rsp status=\"error\" code=\"%s\"/>", responses[id - 1]);
+    Serial.println(response_buffer);
 }
