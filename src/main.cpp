de3d12405b609825b3023f833c690195e6e6e5dd
steve0-t <stefan.simai@mailbox.org>
2026-04-18 22:29:24 +0200

    use main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,31 @@ int main() {
     Serial.begin(115200);
 
     XMLParser parser = XMLParser();
-    rstr input = {._str = (char*)malloc(256 * sizeof(char)),
-                  .len  = 0};
+    rstr      input  = {._str = (char*)malloc(256 * sizeof(char)), .len = 0};
 
+    Retval    res;
     for (;;) {
         if (Serial.available()) {
-            input.len = (u64)Serial.readBytesUntil(
-                '\n', input._str, 256);
-            Serial.println(input._str);
-            if (parser.isXML(input)) {
-                Serial.println("is xml");
-                digitalWrite(A0, HIGH);
-                delay(2000);
-                digitalWrite(A0, LOW);
+            input.len = (u64)Serial.readBytesUntil('\n', input._str, 256);
+            if ((res = parser.parse(input)) < VALID) {
+                Serial.println("invalid!");
+                if (res > 0 && res < VALID) {
+                    char response_buffer[128] = {0};
+                    snprintf(response_buffer, 128,
+                             "<rsp status=\"error\" code=\"%s\"/>",
+                             responses[res - 1]);
+                }
+            } else {
+                if (res == COMMENT)
+                    Serial.println("Comment");
+                Serial.println("valid!");
             }
+            // if (parser.isXML(input)) {
+            //     Serial.println("is xml");
+            //     digitalWrite(A0, HIGH);
+            //     delay(2000);
+            //     digitalWrite(A0, LOW);
+            // }
             input.clear_str();
             // if (input.equals("hello")) {
             //     Serial.println("secret discovered");
@@ -41,6 +52,7 @@ int main() {
             //     digitalWrite(A1, LOW);
             // }
         }
+        free(input._str);
     }
 
     return 0;
