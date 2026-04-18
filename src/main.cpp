7037eb5a1b07c68937ac1b74711e199f3f8d211c
steve0-t <stefan.simai@mailbox.org>
2026-04-18 03:22:27 +0200

    add body of main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "HardwareSerial.h"
+#include "pins_arduino.h"
 #include <Arduino.h>
 #include <avr/interrupt.h>
+#include "../lib/Rstr/Rstr.h"
+#include "../lib/XMLParser/XMLParser.h"
 
 int main() {
     init();
@@ -11,13 +14,32 @@ int main() {
 
     Serial.begin(115200);
 
+    XMLParser parser = XMLParser();
+    rstr input = {._str = (char*)malloc(256 * sizeof(char)),
+                  .len  = 0};
+
     for (;;) {
         if (Serial.available()) {
-            String input = Serial.readStringUntil('\n');
-            Serial.println(input);
-            if (input.equals("hello")) {
-                Serial.println("secret discovered");
+            input.len = (u64)Serial.readBytesUntil(
+                '\n', input._str, 256);
+            Serial.println(input._str);
+            if (parser.isXML(input)) {
+                Serial.println("is xml");
+                digitalWrite(A0, HIGH);
+                delay(2000);
+                digitalWrite(A0, LOW);
             }
+            input.clear_str();
+            // if (input.equals("hello")) {
+            //     Serial.println("secret discovered");
+            //     digitalWrite(A0, HIGH);
+            //     delay(2000);
+            //     digitalWrite(A0, LOW);
+            // } else {
+            //     digitalWrite(A1, HIGH);
+            //     delay(2000);
+            //     digitalWrite(A1, LOW);
+            // }
         }
     }
 
